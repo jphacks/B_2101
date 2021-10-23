a469dc3bedf09b51fa79af31c1237e58f3692619
Leon Kunishi <49759672+Kamenleon@users.noreply.github.com>
2021-10-23 13:13:55 +0900

    add vrm

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("top.html")
+    return render_template("face.html")
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
