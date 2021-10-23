16365c19a81417248ebc3d2af11f1c74214493f0
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-10-23 17:44:46 +0900

    webpack

---
+++ src/entry/js/pages/emotion_test.js
@@ -0,0 +1,5 @@
+// JavaScript
+import '../components/face_test/clmtrackr.js';
+import '../components/face_test/model_pca_20_svm_emotion.js';
+import '../components/face_test/emotionClassifier.js';
+import '../components/face_test/emotionModel.js';
---
+++ src/entry/js/pages/face_test.js
@@ -0,0 +1,3 @@
+// JavaScript
+import '../components/face_test/clmtrackr.js';
+import '../components/face_test/model_pca_20_svm.js';
