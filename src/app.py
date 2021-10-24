f57441f36c932eae2918bd2051a8839f679afe13
Leon Kunishi <49759672+Kamenleon@users.noreply.github.com>
2021-10-24 11:59:20 +0900

    first push

--- src/app.py
+++ src/app.py
@@ -18,5 +18,4 @@ def emotion_test():
     return render_template("test_emotion.html")
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
