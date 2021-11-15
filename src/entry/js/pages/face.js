3edbad1b98fd5eb43367a1a3b8be92dd348a4185
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-11-15 19:21:45 +0900

    delete localStorage test

--- src/entry/js/pages/face.js
+++ src/entry/js/pages/face.js
@@ -2,7 +2,6 @@
 import '../../ts/index.ts';
 
 // JavaScript
-import '../components/localStorage.js';
 import '../components/opening.js';
 import '../components/face_vue.js';
 import '../components/face_func.js';
