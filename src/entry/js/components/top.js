50a9abd012f9c704aae907c49a509aab106176bf
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-11-15 18:50:01 +0900

    fix helloVoice js->vue

--- src/entry/js/components/top.js
+++ src/entry/js/components/top.js
@@ -1,12 +1,3 @@
-const helloVoice = new Audio("./static/sound/voice/Voices_miraikomachi_voice_01.wav");
-
-document.getElementById("sound").onclick = function () {
-  var soundCheck = document.getElementById("sound").checked
-  if (soundCheck == true) {
-    helloVoice.play();
-  }
-}
-
 const top = new Vue({
   el: '#mainArea',
   // FlaskとVueを共存させるためにDelimiterを変更する
@@ -17,6 +8,13 @@ const top = new Vue({
     localStorageRemove: function () {
       localStorage.removeItem('key')
       console.log('localStorageRemove!')
+    },
+    hello: function () {
+      const helloVoice = new Audio("./static/sound/voice/Voices_miraikomachi_voice_01.wav");
+      var soundCheck = document.getElementById("sound").checked
+      if (soundCheck == true) {
+        helloVoice.play();
+      }
     }
   }
 });
