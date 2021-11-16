7b18c6e30c14433a32db69e01a27465b3ca29896
Ryunosuke-Ikeda <ryunosuke0305@gmail.com>
2021-11-16 23:40:10 +0900

    追加応援フラグ

--- src/entry/js/components/face_func.js
+++ src/entry/js/components/face_func.js
@@ -27,7 +27,9 @@ tracker.init(pModel);
 // video 要素内でフェイストラッキング開始
 tracker.start(video);
 
-var ouen_flag = 0
+var ouen_flag_ao = 0
+var ouen_flag_u = 0
+var ouen_flag_ie = 0
 
 // 描画ループ
 function drawLoop() {
@@ -55,20 +57,48 @@ function drawLoop() {
   var abs_dis_x = positions[14][0]-positions[0][0]
 
   var abs_x = Math.round(1000*(positions[50][0] - positions[44][0])/abs_dis_x)
+  var abs_y = Math.round(1000*(positions[53][1] - positions[47][1])/abs_dis_x)
 
   console.log('正規化後の口角の座標');
   console.log('相対x座標(50-44)：「' + abs_x + '」');
+  console.log('相対y座標(53-47)：「' + abs_y + '」');
+  /*
+  console.log('----------------------------------');
+  console.log('53：「' + positions[53][1] + '」');
+  console.log('47：「' + positions[47][1]+ '」');
+  console.log('abs_d：「' + abs_dis_x + '」');
+  */
 
-  
-  if (abs_x>420){
-    ouen_flag=3 //がんばった
-  }else if (abs_x>400){
-    ouen_flag=2 //あとちょっと
-  }else if (abs_x>380){
-    ouen_flag=1 //もっと頑張れ
+  //あ，お 応援フラグ
+  if (abs_y>190){
+    ouen_flag_ao=3 //がんばった
+  }else if (abs_y>170){
+    ouen_flag_ao=2 //あとちょっと
+  }else{
+    ouen_flag_ao=1 //もっと頑張れ
   }
 
-  console.log('応援フラグ:「' + ouen_flag + '」')
+  //う 応援フラグ
+  if (abs_x<350){
+    ouen_flag_u=3 //がんばった
+  }else if (abs_x<370){
+    ouen_flag_u=2 //あとちょっと
+  }else{
+    ouen_flag_u=1 //もっと頑張れ
+  }
+
+  //い，え 応援フラグ
+  if (abs_x>420){
+    ouen_flag_ie=3 //がんばった
+  }else if (abs_x>400){
+    ouen_flag_ie=2 //あとちょっと
+  }else{
+    ouen_flag_ie=1 //もっと頑張れ
+  }
+
+  console.log('あ，お　応援フラグ:「' + ouen_flag_ao + '」')
+  console.log('う　応援フラグ:「' + ouen_flag_u + '」')
+  console.log('い，え　応援フラグ:「' + ouen_flag_ie + '」')
 
 
 
